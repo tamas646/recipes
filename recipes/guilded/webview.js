40c55e05f4c38b770eafc98a7d5d3e1faa9597b5
Jared Kotoff <3268576+jaredkotoff@users.noreply.github.com>
2022-08-14 08:09:19 -0700

    Update Guilded (#875)

--- recipes/guilded/webview.js
+++ recipes/guilded/webview.js
@@ -1,16 +1,43 @@
 module.exports = Ferdi => {
   const getMessages = () => {
-    let unread = 0;
-    const notificationBadge = document.querySelectorAll(
-      '.NavSelectorItem-unread-badge',
-    )[0];
-    if (notificationBadge != undefined) {
-      const innerBadge =
-        notificationBadge.querySelectorAll('.BadgeV2-count')[0];
-      unread = Ferdi.safeParseInt(innerBadge.textContent);
-    }
-    Ferdi.setBadge(unread);
+    const directUnread = getDirectMessageCount();
+    const indirectUnread = getIndirectMessageCount();
+    Ferdi.setBadge(directUnread, indirectUnread);
   };
 
+
+  const getDirectMessageCount = () => {
+    let unread = 0;
+    const notificationBadges = document.querySelectorAll('.NavV4SelectorRow-icon-container');
+    if (notificationBadges?.length) {
+      for(let i = 0; i < notificationBadges.length; i++) {
+        const innerBadge = notificationBadges[i].querySelectorAll('.BadgeV2-count')[0];
+        unread += Ferdi.safeParseInt(innerBadge?.textContent || '0');
+      }
+    }
+
+    return unread;
+  }
+
+  const getIndirectMessageCount = () => {
+    const notificationBadges = document.querySelectorAll('.NavV4SelectorRow-container-unread');
+    return Ferdi.safeParseInt(notificationBadges?.length || '0');
+  }
+
+
+  window.addEventListener('beforeunload', async () => {
+    Ferdi.clearStorageData(settings.id, {
+      storages: [
+        'appcache',
+        'serviceworkers',
+        'cachestorage',
+        'websql',
+        'indexdb',
+      ],
+    });
+    Ferdi.releaseServiceWorkers();
+  });
+
   Ferdi.loop(getMessages);
+
 };
