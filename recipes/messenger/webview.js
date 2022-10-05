234a8cc62910be1727b9cd71e5b38af3025e0261
tamas646 <37243375+tamas646@users.noreply.github.com>
2022-10-05 15:34:49 +0200

    Adapting to UI change (2)

--- recipes/messenger/webview.js
+++ recipes/messenger/webview.js
@@ -7,7 +7,7 @@ module.exports = Ferdi => {
     /*
      * new notification counter by tamas646
      */
-    let notif_indicators = document.querySelectorAll('span.fxk3tzhb.odagglqh.s9ok87oh.s9ljgwtm.lxqftegz.bf1zulr9.k250bvdn.cv5aopd8.frfouenu.bonavkto.djs4p424.r7bn319e.qmqpeqxj.e7u6y3za.qwcclf47.nmlomj2f');
+    let notif_indicators = document.querySelectorAll('span.x3nfvp2.xwnonoy.x13fuv20.xu3j5b3.x1q0q8m5.x26u7qi.x1kpxq89.xsmyaan.x972fbf.xcfux6l.x1qhh985.xm0m39n.x14yjl9h.xudhj91.x18nykt9.xww2gxu');
     if(count_muted) {
       count = notif_indicators.length;
     }
